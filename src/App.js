51ddadf1e5a72cb2dfb26e77b65f87c830d942dd
Carol Giblin <giblinCK@gmail.com>
2018-07-01 17:20:26 -0600

    couting forever

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import pictures from './pics.js';
+import {Chance} from 'chance';
+
+const chance = new Chance();
+let score;
+const win = 8;
+
+// class App extends Component {
+
+//   state = {
+//     pictures: pictures,
+//     score: 0,
+//     win: 8,
+//   }
+
+class App extends Component {
+     constructor(props) {
+       super(props);
+       this.state = {
+         pictures: pictures,
+         score: 0,
+         win: 8,
+         message: ""
+       };
+       this.handleClick = this.handleClick.bind(this);
+     }
+
+  handleClick = () => {
+    this.setState({pictures: chance.shuffle(this.state.pictures)});
+    this.setState({score: this.state.score + 1});
+
+    if (this.setState.score + 1 === win) {
+      console.log("you win")
+    } else {
+      console.log("loser")
+    }
+   console.log("clicked", score)
+  }
+
+  // loser = () => {
+  //   if (this.setState.score + 1 > 8){
+  //     console.log("lost")
+  //   }
+  // }
+
+  render() {
+    const myStyles = {
+      firstClassName: {
+        width: "200px",
+        height: "200px",
+        paddingLeft: "40px",
+        paddingTop: "40px"
+      },
+      secondClassName: {
+        
+      }
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Click-it</h1>
+          <p>Click on the photos below to add to increase your score.  don't click on the same photo twice!</p>
+        </header>
+        < div className = "pictures" >
+        <h3 className="score">Score: {this.state.score}</h3>
+          {this.state.pictures.map(singleDude => {
+              return (
+                <img src={singleDude.image}
+                     style={myStyles.firstClassName}
+                     onClick={this.handleClick} 
+                    //  onClick={this.handleScore} //breaks shuffle, but counts
+                />
+            )
+          })}
+        </div>
+        {/* <h1>Hello {this.state.name}</h1>
+        <button className= "test" onClick={this.handleClick}>click it</button>
+      </div> */}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+//  let clickedPictures: [];
+
+//  class App extends Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        pictures,
+//        clickedPictures
+//      };
+//      this.handleClick = this.handleClick.bind(this);
+//    }
+
+//    // randomize() {
+//    //   console.log("random");
+//    //     var items = this.state.items.slice()
+
+//    //     // Find the index of the selected item within the current items array.
+//    //     var selectedItemName = this.state.selected;
+
+//    //     function isSelectedItem(element, index, array) {
+//    //       return element.id === selectedItemName;
+//    //     };
+//    //     var selectedIdx = items.findIndex(isSelectedItem);
+
+//    //     // Extract that item
+//    //     var selectedItem = items[selectedIdx];
+
+//    //     // Delete the item from the items array
+//    //     items.splice(selectedIdx, 1);
+
+//    //     // Sort the items that are left over
+//    //     items.sort(function (a, b) {
+//    //       return a.id < b.id ? -1 : 1;
+//    //     });
+
+//    //     // Insert the selected item back into the array
+//    //     items.splice(1, 0, selectedItem);
+
+//    //     // Set the state to the new array
+//    //     this.setState({
+//    //       items: items
+//    //     });
+
+//    // }
+
+//    handleClick() {
+//        console.log("clicked");
+//        for (let i = pictures.length - 1; i > 0; i--) {
+//          let j = Math.floor(Math.random() * (i + 1));
+//          [pictures[i], pictures[j]] = [pictures[j], pictures[i]];
+//        }
+
+//        randomize = pictures => {
+//          let clickedPictures = this.state.clickedPictures;
+//          this.setState({
+//            name: "Bob"
+//          })
+//        }
---
+++ src/pics.js
@@ -0,0 +1,43 @@
+import ch1 from './assets/ch1.png';
+import ch2 from './assets/ch2.png';
+import ch3 from './assets/ch3.png';
+import ch4 from './assets/ch4.png';
+import ch5 from './assets/ch5.png';
+import ch6 from './assets/ch6.png';
+import ch7 from './assets/ch7.png';
+import ch8 from './assets/ch8.png';
+
+const pictures =
+    [
+        {
+            id: 1,
+            image: ch1
+        }, {
+            id: 2,
+            image: ch2
+        }, {
+            id: 3,
+            image: ch3
+        }, {
+            id: 4,
+            image: ch4
+        },
+        {
+            id: 5,
+            image: ch5
+        },
+        {
+            id: 6,
+            image: ch6
+        },
+        {
+            id: 7,
+            image: ch7
+        },
+        {
+            id: 8,
+            image: ch8
+        }
+    ]
+
+    export default pictures;
